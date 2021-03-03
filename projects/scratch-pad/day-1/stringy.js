5f5fb351b6859757c644c81bb6a7ca5c567a9419
Ubuntu <ubuntu@ip-172-31-55-146.ec2.internal>
2021-03-03 18:10:45 +0000

    add portfolio.html file

---
+++ projects/scratch-pad/day-1/stringy.js
@@ -0,0 +1,214 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: stringy.js
+ */
+
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+    return string.length;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+    return string.toLowerCase();
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+    return string.toUpperCase();
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+    return string.toLowerCase().split(' ').join('-');
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    let inputStr = string.toLowerCase(); 
+    return inputStr[0] === char.toLowerCase() ? true : false; 
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    
+    return string[string.length - 1].toLowerCase() === char.toLowerCase() ? true : false;
+    // Using the length property to access the last character of the input String,
+    // then comparing it to the input Char using a strict comparison;
+    // both passed-in values case insensitized using the .toLowerCase method. 
+    // The return statement utilizes the Ternary Operator.
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    
+    return stringOne + stringTwo;
+    // mushing together  two passed-in string values using 
+        //the concatenation operator (+)
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    
+    var args = Array.from(arguments);
+    return args.join('');
+    // The .join method is used here without a separator to 
+        // concatenate all input strings
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    
+    return stringOne.length > stringTwo.length ? stringOne : stringTwo; 
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return -1 if the first is higher in alphabetical order than
+ * the second, return 1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    
+    // B/c chars have set Unicode values, we have to render input case insensitive,
+        // as A !== a per Unicode value.
+        return stringOne.toLowerCase() === stringTwo.toLowerCase() ? 0 : 
+        stringOne.toLowerCase() > stringTwo.toLowerCase() ? -1 : 1;
+        
+        // I used a double Ternary statement in the return. 
+            // Working as intended.
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    
+    return  stringOne.toLowerCase() === stringTwo.toLowerCase() ? 0 : 
+            stringOne.toLowerCase() > stringTwo.toLowerCase() ? 1 : -1;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.length = length;
+    module.exports.toLowerCase = toLowerCase;
+    module.exports.toUpperCase = toUpperCase;
+    module.exports.toDashCase = toDashCase;
+    module.exports.beginsWith = beginsWith;
+    module.exports.endsWith = endsWith;
+    module.exports.concat = concat;
+    module.exports.join = join;
+    module.exports.longest = longest;
+    module.exports.sortAscending = sortAscending
+    module.exports.sortDescending = sortDescending;
+}
---
+++ projects/scratch-pad/day-2/loops.js
@@ -0,0 +1,139 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: LOOPS
+ */
+
+/** 
+ * Given an input Array, loop forward over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValues(array) {
+  // YOUR CODE BELOW HERE //
+  for (let i = 0; i <= array.length - 1; i++) { 
+    console.log(array[i]);
+  }
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Array, loop backwards over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValuesInReverse(array) {
+  // YOUR CODE BELOW HERE //
+
+  for (let i = array.length - 1; i >= 0; i--) { 
+    console.log(array[i]); 
+
+  }
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return an Array containing the Object keys.
+ */
+ function getObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+  let keyArr = [];
+  for (var key in object) {
+    //console.log(object[key]);
+    keyArr.push(key); 
+  }
+  return keyArr;
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, loop over the Object and print its keys 
+ * using console.log().
+ */
+function printObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+  //let keyArr = [];
+  for (var key in object) {
+    //console.log(object[key]);
+   console.log(key); 
+  }
+  //return keyArr;
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return an Array containing the Object's values.
+ */
+function getObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+  let valArr = [];
+  for (var key in object) {
+    //console.log(object[key]);
+    valArr.push(object[key]); 
+  }
+  return valArr;
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, loop over the Object and print its values 
+ * using console.log().
+ */
+function printObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+  
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, return the number of key/value pairs stored within that Object.
+ */
+function getObjectLength(object) {
+  // YOUR CODE BELOW HERE //
+  
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input Object, how might we loop over the Object IN REVERSE and 
+ * print its values using console.log()?
+ */
+function printObjectValuesInReverse(object) {
+  // YOUR CODE BELOW HERE //
+  
+  
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.printArrayValues = printArrayValues;
+    module.exports.printArrayValuesInReverse = printArrayValuesInReverse;
+    module.exports.printObjectValues = printObjectValues;
+    module.exports.getObjectValues = getObjectValues;
+    module.exports.getObjectKeys = getObjectKeys;
+    module.exports.printObjectKeys = printObjectKeys;
+    module.exports.getObjectLength = getObjectLength;
+    module.exports.printObjectValuesInReverse = printObjectValuesInReverse;
+}
